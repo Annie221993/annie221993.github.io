f83e0285179ca74550a6993cdfc67d30ce8d614d
Annie221993 <chrislider1993@gmail.com>
2025-12-14 22:37:22 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from mini_turtle_oo import Tortuga
+
+# Crear dos objetos independientes
+t1 = Tortuga()
+t2 = Tortuga()
+
+# Mover cada tortuga
+t1.adelante(5)
+t2.adelante(2)
+
+t1.adelante(3)
+t2.adelante(4)
+
+# Mostrar posiciones finales
+print("Posición final de t1:", t1.posicion_x)
+print("Posición final de t2:", t2.posicion_x
